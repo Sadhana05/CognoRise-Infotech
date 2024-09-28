ad439bf6af14d866b4e64f20496d597432d4d0de
Sadhana <155810661+Sadhana05@users.noreply.github.com>
2024-09-28 14:39:51 +0530

    converter

---
+++ coverter/src/App.jsx
@@ -0,0 +1,9 @@
+import CurrencyConverter from './components/currencyconverter';
+import './App.css';
+
+function App() {
+  return <CurrencyConverter url='http://api.frankfurter.app' />;
+}
+
+export default App;
+
